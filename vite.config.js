e9ddb38f66bebee995c00004df65f01bc67769fa
Ibrahim Al-mahdy <ibrahim.almahdy.contact@gmail.com>
2026-08-02 09:29:45 +0300

    Configure Vite for multi-page deployment

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
+        distributors: resolve(__dirname, "distributors.html"),
+        coffee: resolve(__dirname, "coffee.html"),
+        cappuccino: resolve(__dirname, "cappuccino.html"),
+        "turkish-coffee": resolve(__dirname, "turkish-coffee.html"),
+      },
+    },
+  },
+});
